6a5f92c5aa67ef2c14eed2cd667b0fc48e980519
Michael Soegtrop <7895506+MSoegtropIMC@users.noreply.github.com>
2020-08-10 14:31:44 +0200

    Renamed test/example batch file for MinGW / cygwin and added comment

--- windows/example_call.bat
+++
@@ -1,8 +0,0 @@
-REM Please adjust the below paths as appropriate for your local setup
-
-CALL ..\coq_platform_make_windows.bat ^
-  -destcyg=C:\bin\cygwin_coqplatform_8_11_2 ^
-  -cygcache=C:\bin\setupfiles\cygwin_cache ^
-  -cygrepo=https://mirrors.kernel.org/sourceware/cygwin
-
-REM -cygquiet=N   -cygforce=Y
