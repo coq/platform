0059456867e73a18e59002cdee0e8919e387952d
Michael Soegtrop <7895506+MSoegtropIMC@users.noreply.github.com>
2020-08-10 12:04:01 +0200

    Removed script based pre-requisite package build system + cleaned up cygwin MinGW build

--- windows/example_call.bat
+++ windows/example_call.bat
@@ -3,7 +3,6 @@ REM Please adjust the below paths as appropriate for your local setup
 CALL ..\coq_platform_make_windows.bat ^
   -destcyg=C:\bin\cygwin_coqplatform_8_11_2 ^
   -cygcache=C:\bin\setupfiles\cygwin_cache ^
-  -srccache=C:\bin\setupfiles\source_cache ^
-  -cygrepo=http://mirror.checkdomain.de/cygwin
+  -cygrepo=https://mirrors.kernel.org/sourceware/cygwin
 
 REM -cygquiet=N   -cygforce=Y
