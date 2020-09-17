9d0968960cf073542e623c52b19ad76f386a4b38
Michael Soegtrop <7895506+MSoegtropIMC@users.noreply.github.com>
2020-09-17 21:49:44 +0200

    Editorial changes in messages

--- shell_scripts/ask_vst.sh
+++ shell_scripts/ask_vst.sh
@@ -15,7 +15,7 @@
 if [ -z "${COQ_PLATFORM_VST:+x}" ] && [[ "${COQ_PLATFORM_COMPCERT}"  =~ ^[fFoO] ]]
 then
 cat <<EOH
-======================= Verified Software Toolchain VST =======================
+======================= VERIFIED SOFTWARE TOOLCHAIN VST =======================
 The Coq platform installs the Verified Software Toolchain VST.
 
 Unfortunately VST takes a while to compile - on a fast machine with 32GB RAM it
@@ -26,7 +26,7 @@ In case you do not plan to formally verify C code with VST, you might want
 to select no (n) below. You can install VST any time later with:
 
   opam install coq-vst.2.6.
-======================= Verified Software Toolchain VST =======================
+======================= VERIFIED SOFTWARE TOOLCHAIN VST =======================
 EOH
   ask_user_opt2_cancel "Install VST (y) or do not install VST (n)?" yY "install VST" nN "do not install VST"
   COQ_PLATFORM_VST=$ANSWER
