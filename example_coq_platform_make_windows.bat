f8debe695c704b9455015762c90381fe936bc698
Michael Soegtrop <7895506+MSoegtropIMC@users.noreply.github.com>
2020-09-18 16:17:04 +0200

    Changed default path in Windows example run file

--- example_coq_platform_make_windows.bat
+++ example_coq_platform_make_windows.bat
@@ -5,7 +5,7 @@ REM The reason for having a version specific cygwin folder name is simplificatio
 
 CALL coq_platform_make_windows.bat ^
   -destcyg=C:\bin\cygwin_coqplatform_8_12_0 ^
-  -cygcache=C:\bin\setupfiles\cygwin_cache ^
+  -cygcache=C:\bin\cygwin_cache ^
   -cygrepo=https://mirrors.kernel.org/sourceware/cygwin ^
   -intro=n -parallel=p -jobs=16 -compcert=o -vst=y -switch=k
 
