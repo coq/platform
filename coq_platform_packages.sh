504eebde8e6285688d05ea1f03da85d3bbfb3ebe
Michael Soegtrop <7895506+MSoegtropIMC@users.noreply.github.com>
2020-12-06 11:15:30 +0100

    Bumped Coq version to 8.12.1 and platform version to 8.12.1.0

--- coq_platform_packages.sh
+++ coq_platform_packages.sh
@@ -15,11 +15,11 @@
 # ATTENTION: The packages are given in an oeder so that dependencies are built
 #            first - important fo sequential builds!
 
-PACKAGES="coq.8.12.0"
+PACKAGES="coq.8.12.1"
 
 # GTK based IDE for Coq - alternatives are VSCoq and Proofgeneral for Emacs
 # Note: lablgtk3 3.1.1 does not link with flexlink on MinGW
-PACKAGES="${PACKAGES} coqide.8.12.0 lablgtk3.3.0.beta5"
+PACKAGES="${PACKAGES} coqide.8.12.1 lablgtk3.3.0.beta5"
 
 # Some generally useful packages
 PACKAGES="${PACKAGES} coq-unicoq.1.5+8.12"
