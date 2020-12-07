88562c61511364997435eb61be5aadb1a2d47dde
Enrico Tassi <Enrico.Tassi@Inria.fr>
2020-12-07 14:39:42 +0100

    pick coq and coqide version 8.13+beta1

--- coq_platform_packages.sh
+++ coq_platform_packages.sh
@@ -15,12 +15,12 @@
 # ATTENTION: The packages are given in an oeder so that dependencies are built
 #            first - important fo sequential builds!
 
-PACKAGES="coq.8.13.dev"
+PACKAGES="coq.8.13+beta1"
 
 # GTK based IDE for Coq - alternatives are VSCoq and Proofgeneral for Emacs
 if  [[ "${COQ_PLATFORM_EXTENT}"  =~ ^[iIfF] ]]
 then
-PACKAGES="${PACKAGES} coqide.8.13.dev lablgtk3.3.1.1"
+PACKAGES="${PACKAGES} coqide.8.13+beta1 lablgtk3.3.1.1"
 fi
 
 if  [[ "${COQ_PLATFORM_EXTENT}"  =~ ^[fF] ]]
