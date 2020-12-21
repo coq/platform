ee312fc4fa36aebf610d27f6fa3f349b9e2e40de
Michael Soegtrop <7895506+MSoegtropIMC@users.noreply.github.com>
2020-12-21 12:37:06 +0100

    Windows: improved comments in script calling example

--- example_coq_platform_make_windows.bat
+++ example_coq_platform_make_windows.bat
@@ -1,12 +1,23 @@
 REM This is a test / example call for coq_platform_make_windows.bat.
-REM Please note that it is no problem to install several coq-platform version into one cygwin.
+REM Please note that it is no problem to install several coq-platform versions into one cygwin.
 REM Each coq-platform will create its own opam switch.
-REM The reason for having a version specific cygwin folder name is simplification of testing.
+REM But you do need separate cygwin folders for compiling a 64 bit and 32 bit Coq (-arch=32).
 
 CALL coq_platform_make_windows.bat ^
-  -destcyg=C:\cygwin_coq_platform ^
+  -arch=64 ^
+  -destcyg=C:\cygwin64_coq_platform ^
   -cygcache=C:\bin\cygwin_cache ^
   -cygrepo=https://mirrors.kernel.org/sourceware/cygwin ^
   -extent=f -parallel=p -jobs=16 -switch=k -compcert=o -vst=y
 
-REM -cygquiet=N -cygforce=Y
+REM NOTE: You can also call coq_platform_make_windows.bat from a cygwin prompt
+REM (e.g. in case you use cygwin for GIT) but please remember to quote paths with
+REM back slashes with single quotes then!
+REM The above command would be at a cygwin shell prompt:
+REM
+REM   ./coq_platform_make_windows.bat \
+REM     -arch=64 \
+REM     -destcyg='C:\cygwin_coq_platform' \
+REM     -cygcache='C:\bin\cygwin_cache' \
+REM     -cygrepo=https://mirrors.kernel.org/sourceware/cygwin \
+REM     -extent=f -parallel=p -jobs=16 -switch=k -compcert=o -vst=y
