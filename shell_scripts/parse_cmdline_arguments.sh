989690e2ac687a2ad88003b98aa9a70c4cb71bf4
Michael Soegtrop <7895506+MSoegtropIMC@users.noreply.github.com>
2021-04-18 09:57:52 +0200

    Added support for choosing between different Coq versions - Added package list selection question - Added package list for Coq 8.12.2 - Added closing comment since the full opam switch name is only known after package selection

--- shell_scripts/parse_cmdline_arguments.sh
+++ shell_scripts/parse_cmdline_arguments.sh
@@ -12,17 +12,17 @@ function print_usage {
 cat <<"EOH"
 coq_platform_make.sh [options]
 
-Create a new opam switch named $COQ_PLATFORM_SWITCH_NAME
-and make and install the Coq platform in this switch.
+Create a new opam switch and make and install the Coq Platform in this switch.
 
 If an option is not given, the option is explained and asked for interactively.
 Except for expert users this is the recommended way to run this script.
 
 OPTIONS:
   -h, -help    Print this help message
-  -extent=p    Setup opam and build full Coq platform
-  -extent=c    Just setup opam and build Coq
+  -extent=f    Setup opam and build full Coq platform
+  -extent=b    Just setup opam and build Coq
   -extent=i    Just setup opam and build Coq+CoqIDE
+  -packages=file  Select the package list / version file
   -parallel=p  Build several opam packages in parallel
   -parallel=s  Build opam packages sequentially
   -jobs=1..16  Number of make threads per package
@@ -39,20 +39,22 @@ EOH
 for arg in "$@"
 do
   case "$arg" in
-    -help|-h)    print_usage; false;;
-    -extent=*)   COQ_PLATFORM_EXTENT="${arg#*=}";;
-    -parallel=*) COQ_PLATFORM_PARALLEL="${arg#*=}";;
-    -jobs=*)     COQ_PLATFORM_JOBS="${arg#*=}";;
-    -compcert=*) COQ_PLATFORM_COMPCERT="${arg#*=}";;
-    -vst=*)      COQ_PLATFORM_VST="${arg#*=}";;
-    -switch=*)   COQ_PLATFORM_SWITCH="${arg#*=}";;
+    -help|-h)     print_usage; false;;
+    -extent=*)    COQ_PLATFORM_EXTENT="${arg#*=}";;
+    -packages=*)  COQ_PLATFORM_PACKAGELIST="${arg#*=}";;
+    -parallel=*)  COQ_PLATFORM_PARALLEL="${arg#*=}";;
+    -jobs=*)      COQ_PLATFORM_JOBS="${arg#*=}";;
+    -compcert=*)  COQ_PLATFORM_COMPCERT="${arg#*=}";;
+    -vst=*)       COQ_PLATFORM_VST="${arg#*=}";;
+    -switch=*)    COQ_PLATFORM_SWITCH="${arg#*=}";;
     *) echo "ERROR: Unknown command line argument $arg!"; print_usage; false;;
   esac
 done
 
-check_value_enumeraton "${COQ_PLATFORM_EXTENT:-__unset__}"   "[fbi]" "-extent/COQ_PLATFORM_EXTENT"
-check_value_enumeraton "${COQ_PLATFORM_PARALLEL:-__unset__}" "[ps]"  "-parallel/COQ_PLATFORM_PARALLEL"
-check_value_range      "${COQ_PLATFORM_JOBS:-__unset__}"     1 16    "-jobs/COQ_PLATFORM_JOBS"
-check_value_enumeraton "${COQ_PLATFORM_COMPCERT:-__unset__}" "[fon]" "-compcert/COQ_PLATFORM_COMPCERT"
-check_value_enumeraton "${COQ_PLATFORM_VST:-__unset__}"      "[yn]"  "-vst/COQ_PLATFORM_VST"
-check_value_enumeraton "${COQ_PLATFORM_SWITCH:-__unset__}"   "[kd]"  "-switch/COQ_PLATFORM_SWITCH"
+check_value_enumeraton  "${COQ_PLATFORM_EXTENT:-__unset__}"   "[fbi]" "-extent/COQ_PLATFORM_EXTENT"
+check_value_enumeraton  "${COQ_PLATFORM_PARALLEL:-__unset__}" "[ps]"  "-parallel/COQ_PLATFORM_PARALLEL"
+check_value_range       "${COQ_PLATFORM_JOBS:-__unset__}"     1 16    "-jobs/COQ_PLATFORM_JOBS"
+check_value_enumeraton  "${COQ_PLATFORM_COMPCERT:-__unset__}" "[fon]" "-compcert/COQ_PLATFORM_COMPCERT"
+check_value_enumeraton  "${COQ_PLATFORM_VST:-__unset__}"      "[yn]"  "-vst/COQ_PLATFORM_VST"
+check_value_enumeraton  "${COQ_PLATFORM_SWITCH:-__unset__}"   "[kd]"  "-switch/COQ_PLATFORM_SWITCH"
+check_value_file_exists "${COQ_PLATFORM_PACKAGELIST:-__unset__}" "-packages/COQ_PLATFORM_PACKAGELIST"
