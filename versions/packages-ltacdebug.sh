bf3acee0844f95afc38ed423216c2960b8e49797
Michael Soegtrop <7895506+MSoegtropIMC@users.noreply.github.com>
2021-09-08 18:52:04 +0200

    Added CompCert, VST and its prerequisites to 8.14 variant (and without VST to ltacdebug variant)

--- versions/packages-ltacdebug.sh
+++ versions/packages-ltacdebug.sh
@@ -39,8 +39,8 @@ then
 PACKAGES="${PACKAGES} coqide.dev~ltacdebug lablgtk3.3.1.1"
 fi
 
-# if  [[ "${COQ_PLATFORM_EXTENT}"  =~ ^[fF] ]]
-# then
+if  [[ "${COQ_PLATFORM_EXTENT}"  =~ ^[fF] ]]
+then
 
 # # Some generally useful packages
 # PACKAGES="${PACKAGES} coq-unicoq.1.5+8.13"
@@ -52,10 +52,10 @@ fi
 # PACKAGES="${PACKAGES} coq-simple-io.1.5.0"
 # PACKAGES="${PACKAGES} coq-quickchick.1.5.0"
 
-# # Analysis and numerics
-# PACKAGES="${PACKAGES} coq-flocq.3.3.1"
-# PACKAGES="${PACKAGES} coq-coquelicot.3.1.0"
-# PACKAGES="${PACKAGES} coq-gappa.1.4.6 gappa.1.3.5"
+# Analysis and numerics
+PACKAGES="${PACKAGES} coq-flocq.3.4.2"
+# PACKAGES="${PACKAGES} coq-coquelicot.3.2.0"
+# PACKAGES="${PACKAGES} coq-gappa.1.4.6 gappa.1.3.5"                            # There is a gappa 1.4.0
 # PACKAGES="${PACKAGES} coq-interval.4.1.1"
 
 # # Elpi, Coq-elpi and hierarchy builder
@@ -77,19 +77,20 @@ fi
 # # Homotopy Type Theory (HoTT)
 # PACKAGES="${PACKAGES} coq-hott.8.13"
 
-# # Menhir, CompCert and Princeton VST - these take longer to compile !
-# PACKAGES="${PACKAGES} coq-menhirlib.20200624 menhir.20200624"
-# # Todo: there is no mutex between coq platform and coq platform open source
-# case "$COQ_PLATFORM_COMPCERT" in
-#   [yY]) PACKAGES="${PACKAGES} coq-compcert.3.8" ;;
-#   [nN]) true ;;
-#   *) echo "Illegal value for COQ_PLATFORM_COMPCERT - aborting"; false ;;
-# esac
+# Menhir, CompCert and Princeton VST - these take longer to compile !
+PACKAGES="${PACKAGES} coq-menhirlib.20210419 menhir.20210419"                   # This is the latest tag
+
+# Todo: there is no mutex between coq platform and coq platform open source
+case "$COQ_PLATFORM_COMPCERT" in
+  [yY]) PACKAGES="${PACKAGES} coq-compcert.3.9~flex" ;;
+  [nN]) true ;;
+  *) echo "Illegal value for COQ_PLATFORM_COMPCERT - aborting"; false ;;
+esac
 
 # case "$COQ_PLATFORM_VST" in
-#   [yY]) PACKAGES="${PACKAGES} coq-vst.2.7.1" ;;
+#   [yY]) PACKAGES="${PACKAGES} coq-vst.2.8~flex" ;;
 #   [nN]) true ;;
 #   *) echo "Illegal value for COQ_PLATFORM_VST - aborting"; false ;;
 # esac
 
-# fi
+fi
