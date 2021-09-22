9d719f6266c4ff6101cc73181b08c0844317ff09
Michael Soegtrop <7895506+MSoegtropIMC@users.noreply.github.com>
2021-09-22 17:04:51 +0200

    Version 8.14: enabled all packages which have a working or trivial to patch opam package

--- versions/packages-8.14.sh
+++ versions/packages-8.14.sh
@@ -47,42 +47,44 @@ if  [[ "${COQ_PLATFORM_EXTENT}"  =~ ^[fF] ]]
 then
 
 # # Some generally useful packages
-# PACKAGES="${PACKAGES} coq-unicoq.1.5+8.13"
-# PACKAGES="${PACKAGES} coq-ext-lib.0.11.3"
-# PACKAGES="${PACKAGES} coq-equations.1.2.3+8.13"
-# PACKAGES="${PACKAGES} coq-bignums.8.13.0"
-# PACKAGES="${PACKAGES} coq-aac-tactics.8.13.0"
-# PACKAGES="${PACKAGES} coq-mtac2.1.4+8.13"
-# PACKAGES="${PACKAGES} coq-simple-io.1.5.0"
-# PACKAGES="${PACKAGES} coq-quickchick.1.5.0"
+#PACKAGES="${PACKAGES} coq-unicoq.1.5+8.13"         # coq-unicoq.1.5+8.13~flex does not compile
+PACKAGES="${PACKAGES} coq-ext-lib.dev"
+#PACKAGES="${PACKAGES} coq-equations.1.2.3+8.13"    # coq-equations.1.2.4+8.13~flex does not compile
+PACKAGES="${PACKAGES} coq-bignums.8.14.0"
+#PACKAGES="${PACKAGES} coq-aac-tactics.8.13.0"      # coq-aac-tactics.8.13.0~flex does not compile
+#PACKAGES="${PACKAGES} coq-mtac2.1.4+8.13"          # depends on unicoq
+PACKAGES="${PACKAGES} coq-simple-io.1.5.0~flex"
 
 # Analysis and numerics
 PACKAGES="${PACKAGES} coq-flocq.3.4.2"
-# PACKAGES="${PACKAGES} coq-coquelicot.3.2.0"
-# PACKAGES="${PACKAGES} coq-gappa.1.4.6 gappa.1.3.5"                            # There is a gappa 1.4.0
-# PACKAGES="${PACKAGES} coq-interval.4.1.1"
+#PACKAGES="${PACKAGES} coq-gappa.1.4.6 gappa.1.3.5" # coq-gappa.1.4.6~flex does not compile, gappa is fine
 
-# # Elpi, Coq-elpi and hierarchy builder
-# PACKAGES="${PACKAGES} coq-elpi.1.8.1 elpi.1.12.0"
-# PACKAGES="${PACKAGES} coq-hierarchy-builder.1.0.0"
+# Elpi, Coq-elpi and hierarchy builder
+#PACKAGES="${PACKAGES} coq-elpi.1.11.0 elpi.1.13.7" # coq-elpi.1.11.0~flex does not compile, elpi is fine
+#PACKAGES="${PACKAGES} coq-hierarchy-builder.1.1.0" # requires above
 
-# # The standard set of mathcomp modules
-# PACKAGES="${PACKAGES} coq-mathcomp-ssreflect.1.12.0"
-# PACKAGES="${PACKAGES} coq-mathcomp-fingroup.1.12.0"
-# PACKAGES="${PACKAGES} coq-mathcomp-algebra.1.12.0"
-# PACKAGES="${PACKAGES} coq-mathcomp-solvable.1.12.0"
-# PACKAGES="${PACKAGES} coq-mathcomp-field.1.12.0"
-# PACKAGES="${PACKAGES} coq-mathcomp-character.1.12.0"
-# # Plus a few extra mathcomp modules
-# PACKAGES="${PACKAGES} coq-mathcomp-bigenough.1.0.0"
-# PACKAGES="${PACKAGES} coq-mathcomp-finmap.1.5.1"
-# PACKAGES="${PACKAGES} coq-mathcomp-real-closed.1.1.2"
+# The standard set of mathcomp modules
+PACKAGES="${PACKAGES} coq-mathcomp-ssreflect.1.12.0"
+PACKAGES="${PACKAGES} coq-mathcomp-fingroup.1.12.0"
+PACKAGES="${PACKAGES} coq-mathcomp-algebra.1.12.0"
+PACKAGES="${PACKAGES} coq-mathcomp-solvable.1.12.0"
+PACKAGES="${PACKAGES} coq-mathcomp-field.1.12.0"
+PACKAGES="${PACKAGES} coq-mathcomp-character.1.12.0"
+# Plus a few extra mathcomp modules
+PACKAGES="${PACKAGES} coq-mathcomp-bigenough.1.0.0"
+PACKAGES="${PACKAGES} coq-mathcomp-finmap.1.5.1"
+PACKAGES="${PACKAGES} coq-mathcomp-real-closed.1.1.2"
+
+# Packages depending on mathcomp
+PACKAGES="${PACKAGES} coq-coquelicot.3.2.0"
+#PACKAGES="${PACKAGES} coq-interval.4.1.1"          # passes opam checks but does not compile
+#PACKAGES="${PACKAGES} coq-quickchick.1.5.0"        # coq-quickchick.1.5.0~flex does not compile
 
 # # Homotopy Type Theory (HoTT)
-# PACKAGES="${PACKAGES} coq-hott.8.13"
+PACKAGES="${PACKAGES} coq-hott.8.13~flex"
 
 # Menhir, CompCert and Princeton VST - these take longer to compile !
-PACKAGES="${PACKAGES} coq-menhirlib.20210419 menhir.20210419"                   # This is the latest tag
+PACKAGES="${PACKAGES} coq-menhirlib.20210419 menhir.20210419"
 
 # Todo: there is no mutex between coq platform and coq platform open source
 case "$COQ_PLATFORM_COMPCERT" in
