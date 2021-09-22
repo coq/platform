9d127cc20fbc8c6bcd345a9da387161ae421b0fd
Michael Soegtrop <7895506+MSoegtropIMC@users.noreply.github.com>
2021-09-22 14:29:46 +0200

    Added script to create tag request issues for included packages

--- maintainer_scripts/notify-upstream-pins.sh
+++ maintainer_scripts/notify-upstream-pins.sh
@@ -29,48 +29,32 @@ esac
 
 ##### Settings #####
 
-DATEBETA="Septamber 17, 2021"
-DATEFINAL="October 31, 2021"
-CC="CC: https://github.com/coq/coq/issues/12334"
+EXT_PRE="+rc1"
+DATE_PRE="Septamber 17, 2021"
+DATE_FINAL="October 31, 2021"
+#CC="CC: https://github.com/coq/coq/issues/12334"
 #CC="\n@coqbot column:...."
 REASON="bundled in the Coq Platform"
 
-##### Read in package list file #####
+##### Shell functions for translating Coq Platform (Opam) package names to Coq CI package names ####
 
-COQ_PLATFORM_EXTENT=f
-COQ_PLATFORM_COMPCERT=y
-COQ_PLATFORM_VST=y
-source $1
+# This function expects $package to be set to a Coq Platform package name and
+# sets package_ci to the corresponding Coq CI package name
 
-COQ_PLATFORM_COQ_BASE_VERSION=${COQ_PLATFORM_COQ_BRANCH#v}
-
-echo COQ_PLATFORM_COQ_BASE_VERSION = "${COQ_PLATFORM_COQ_BASE_VERSION}"
-echo COQ_PLATFORM_COQ_BRANCH       = "${COQ_PLATFORM_COQ_BRANCH}"
-echo COQ_PLATFORM_COQ_TAG          = "${COQ_PLATFORM_COQ_TAG}"
-echo PACKAGES                      = "${PACKAGES}"
-
-##### Get ci-basic-overlay.sh file from the branch corresponding to the version #####
-
-# wget https://raw.githubusercontent.com/coq/coq/${COQ_PLATFORM_COQ_BRANCH}/dev/ci/ci-basic-overlay.sh -O /tmp/branch-ci-basic-overlay.sh
-# wget https://raw.githubusercontent.com/coq/coq/master/dev/ci/ci-basic-overlay.sh -O /tmp/master-ci-basic-overlay.sh
-
-##### Translate (and filter) Coq Platform (= Opam) package names to Coq CI package names ####
-
-ci_packages=''
-
-for package in ${PACKAGES}
-do
-    package_main=${package%%.*}
-    case $package_main in
-    coq)    ci_package='' ;;
-    coqide) ci_package='' ;;
-    coq-*)  ci_package="${package_main#coq-}" ;;
-    *)      ci_package='' ;;
-    esac
-    ci_packages="${ci_packages} ${ci_package}"
-done
-
-echo ci_packages = "${ci_packages}"
+function package_platform_to_ci() {
+  package_main=${package%%.*}
+  package_ci=''
+  case $package_main in
+    coq)                    ;;
+    coqide)                 ;;
+    coq-interval)           ;;
+    coq-mathcomp-ssreflect) package_ci='mathcomp' ;; 
+    coq-mathcomp-*)         ;; 
+    coq-*)                  package_ci="${package_main#coq-}" ;;
+    *)                      ;;
+  esac
+  package_ci=${package_ci//-/_}
+}
 
 ##### Shell functions for reading ci-basic-overlay.sh #####
 
@@ -79,7 +63,7 @@ function read_from() {
   ( . $1; varname="$2"; echo ${!varname} )
 }
 
-##### Shell functions for creating issue URL #####
+##### Shell functions for creating GIT server issue URL #####
 
 # https://gist.github.com/cdown/1163649
 function urlencode() {
@@ -103,18 +87,18 @@ function urlencode() {
 }
 
 function template {
-  TITLE="Please create a tag for the upcoming release of Coq $COQ_PLATFORM_COQ_BASE_VERSION"
-  BODY="The Coq team is planning to release Coq $COQ_PLATFORM_COQ_BASE_VERSION-beta1 on $DATEBETA,
-and Coq $COQ_PLATFORM_COQ_BASE_VERSION.0 on $DATEFINAL.
+  TITLE="Please create a tag for the upcoming release of Coq ${COQ_PLATFORM_COQ_BASE_VERSION}"
+  BODY="The Coq team released Coq ${COQ_PLATFORM_COQ_BASE_VERSION}${EXT_PRE} on $DATE_PRE,
+and plans to release Coq $COQ_PLATFORM_COQ_BASE_VERSION.0 on $DATE_FINAL.
 
 Your project is currently scheduled for being $REASON.
 
-We are currently testing commit $3
+Coq CI is currently testing commit $3
 on branch $1/tree/$2
 but we would like to ship a released version instead (a tag in git's slang).
 
-Could you please tag that commit, or communicate us any other tag
-that works with the Coq branch v$COQ_PLATFORM_COQ_BASE_VERSION at the *latest* 15 days before the
+Could you please create a tag, or communicate us any existing tag
+that works with the Coq branch ${COQ_PLATFORM_COQ_BRANCH} at the *latest* 15 days before the
 date of the final release?
 
 Thanks!
@@ -145,23 +129,53 @@ $CC
   esac
 }
 
+##### Read package list file #####
+
+COQ_PLATFORM_EXTENT=f
+COQ_PLATFORM_COMPCERT=y
+COQ_PLATFORM_VST=y
+
+source $1
+
+COQ_PLATFORM_COQ_BASE_VERSION=${COQ_PLATFORM_COQ_BRANCH#v}
+
+echo COQ_PLATFORM_COQ_BASE_VERSION = "${COQ_PLATFORM_COQ_BASE_VERSION}"
+echo COQ_PLATFORM_COQ_BRANCH       = "${COQ_PLATFORM_COQ_BRANCH}"
+echo COQ_PLATFORM_COQ_TAG          = "${COQ_PLATFORM_COQ_TAG}"
+echo PACKAGES                      = "${PACKAGES}"
+
+##### Get ci-basic-overlay.sh file from the branch corresponding to the version #####
+
+wget https://raw.githubusercontent.com/coq/coq/${COQ_PLATFORM_COQ_BRANCH}/dev/ci/ci-basic-overlay.sh -O /tmp/branch-ci-basic-overlay.sh
+wget https://raw.githubusercontent.com/coq/coq/master/dev/ci/ci-basic-overlay.sh -O /tmp/master-ci-basic-overlay.sh
+
+##### Main loop #####
+
 set +o nounset
 
-for addon in ${ci_packages}
+for package in ${PACKAGES}
 do
+  package_platform_to_ci
+  if [ -n "${package_ci}" ]
+  then
     echo -e "\n----------------------------------------------"
-    URL=`read_from /tmp/master-ci-basic-overlay.sh "${addon}_CI_GITURL"`
-    REF=`read_from /tmp/master-ci-basic-overlay.sh "${addon}_CI_REF"`
-    PIN=`read_from /tmp/branch-ci-basic-overlay.sh "${addon}_CI_REF"`
-    if [ "${#PIN}" = "40" ]; then
-      echo -e "Addon $addon is pinned to a hash, to open an issue open the following url:\n"
-      template $URL $REF $PIN
-    elif [ "${#PIN}" = "0" ]; then
-      echo "Addon $addon has no pin!"
-      exit 1
+    url=`read_from /tmp/master-ci-basic-overlay.sh "${package_ci}_CI_GITURL"`
+    ref=`read_from /tmp/master-ci-basic-overlay.sh "${package_ci}_CI_REF"`
+    pin=`read_from /tmp/branch-ci-basic-overlay.sh "${package_ci}_CI_REF"`
+    echo "Available tags for ${package} are:"
+    git -c 'versionsort.suffix=-' ls-remote --refs --tags --sort='v:refname' "${url}" | sed 's|.*refs/tags/||'
+    if [ "${#pin}" = "40" ]
+    then
+      echo -e "Package ${package} is pinned to a hash in Coq CI, to open an issue open the following url:\n"
+      template ${url} $ref $pin
+    elif [ "${#pin}" = "0" ]
+    then
+      echo "=================================================="
+      echo "ERROR: Package ${package} has no pin!"
+      echo "=================================================="
     else
-      echo "Addon $addon is already pinned to version $PIN"
-      echo "Available versions for $addon matching $PIN or later are:"
-      git -c 'versionsort.suffix=-' ls-remote --refs --tags --sort='v:refname' "${URL}" | sed 's|.*refs/tags/||' | sed -n "/$PIN/,$$p"
+      echo "Package ${package} is already pinned to version $pin"
+      echo 
     fi
+  fi
 done
