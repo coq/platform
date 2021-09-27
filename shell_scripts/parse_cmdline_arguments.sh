c4d082064eaf4f0eccad0d2f2a1168cfa79e8d16
Michael Soegtrop <7895506+MSoegtropIMC@users.noreply.github.com>
2021-09-27 19:33:36 +0200

    Snap: editorial rework and minor fixes - reuse coq_platform_make.sh where possible - document phases - clearer name for environment variables

--- shell_scripts/parse_cmdline_arguments.sh
+++ shell_scripts/parse_cmdline_arguments.sh
@@ -35,6 +35,7 @@ OPTIONS:
   -switch=k    In case the opam switch already exists, keep it
   -switch=d    In case the opam switch already exists, delete it
   -dumplogs    Dump all log files in case of an error (intended for CI)
+  -opamonly    Stop the script after installing opam
 EOH
 }
 
@@ -50,6 +51,7 @@ do
     -vst=*)       COQ_PLATFORM_VST="${arg#*=}";;
     -switch=*)    COQ_PLATFORM_SWITCH="${arg#*=}";;
     -dumplogs)    COQ_PLATFORM_DUMP_LOGS=y;;
+    -opamonly)    COQ_PLATFORM_OPAM_ONLY=y;;
     *) echo "ERROR: Unknown command line argument $arg!"; print_usage; false;;
   esac
 done
@@ -79,5 +81,6 @@ check_value_range       "${COQ_PLATFORM_JOBS:-__unset__}"     1 16    "-jobs/COQ
 check_value_enumeraton  "${COQ_PLATFORM_COMPCERT:-__unset__}" "[yn]"  "-compcert/COQ_PLATFORM_COMPCERT"
 check_value_enumeraton  "${COQ_PLATFORM_VST:-__unset__}"      "[yn]"  "-vst/COQ_PLATFORM_VST"
 check_value_enumeraton  "${COQ_PLATFORM_SWITCH:-__unset__}"   "[kd]"  "-switch/COQ_PLATFORM_SWITCH"
-check_value_enumeraton  "${COQ_PLATFORM_DUMP_LOGS:-__unset__}" "[yn]"  "COQ_PLATFORM_DUMP_LOGS"
+check_value_enumeraton  "${COQ_PLATFORM_DUMP_LOGS:-__unset__}" "[yn]"  "-dumplogs/COQ_PLATFORM_DUMP_LOGS"
+check_value_enumeraton  "${COQ_PLATFORM_OPAM_ONLY:-__unset__}" "[yn]"  "-opamonly/COQ_PLATFORM_OPAM_ONLY"
 check_value_file_exists "${COQ_PLATFORM_PACKAGELIST:-__unset__}" "-packages/COQ_PLATFORM_PACKAGELIST"
