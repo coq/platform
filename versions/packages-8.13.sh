7d66804da5b2df9e1bd68c34bc011949201a660e
Michael Soegtrop <7895506+MSoegtropIMC@users.noreply.github.com>
2021-10-02 17:06:31 +0200

    Version 8.13: Update coq-hammer-tactics to version 1.3.2+8.13

--- versions/packages-8.13.sh
+++ versions/packages-8.13.sh
@@ -106,7 +106,7 @@ then
   PACKAGES="${PACKAGES} coq-elpi.1.11.1 elpi.1.13.7" # Note: coq-elpi 1.11.2 is >=8.14
   PACKAGES="${PACKAGES} coq-hierarchy-builder.1.2.0"
   PACKAGES="${PACKAGES} coq-quickchick.1.5.1"
-  PACKAGES="${PACKAGES} coq-hammer-tactics.1.3.1+8.13"
+  PACKAGES="${PACKAGES} coq-hammer-tactics.1.3.2+8.13"
   PACKAGES="${PACKAGES} coq-paramcoq.1.1.3+coq8.13"
   PACKAGES="${PACKAGES} coq-coqeal.1.0.6"
   PACKAGES="${PACKAGES} coq-libhyps.2.0.3"
