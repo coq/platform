831f1afbc1898a0c6d4a7b558066083c9fc09627
Michael Soegtrop <7895506+MSoegtropIMC@users.noreply.github.com>
2022-03-26 14:15:40 +0100

    Smoke Test: made smoke test file patches package dependent

--- shell_scripts/create_smoke_test_kit.sh
+++ shell_scripts/create_smoke_test_kit.sh
@@ -46,91 +46,101 @@ packages="$(opam list --installed-roots --short --columns=name | grep "${pattern
 
 ##### Associate package name with test/example file(s) #####
 
+# A space sparated list of file paths in the package source tree [requires]
 declare -A TEST_FILES
+# coqc options required to run the test, typically -Q or -R options [optional]
 declare -A COQ_OPTION
+# A AWK command for patching test files (typically require statements) [optional]
+declare -A PATCH_CMDS
 
 TEST_FILES[coq-aac-tactics]='theories/Tutorial.v'
 TEST_FILES[coq-bignums]='tests/success/bigQ.v tests/success/NumberScopes.v'
-TEST_FILES[coq-compcert]='lib/Coqlib.v'
 TEST_FILES[coq-compcert-32]='lib/Coqlib.v'
 COQ_OPTION[coq-compcert-32]='-Q $COQLIB/../coq-variant/compcert32/compcert compcert'
+TEST_FILES[coq-compcert]='lib/Coqlib.v'
+TEST_FILES[coq-coqeal]='refinements/examples/irred.v'
+TEST_FILES[coq-coqprime-generator]=''
+TEST_FILES[coq-coqprime]='src/Coqprime/examples/russell1.v'
+PATCH_CMDS[coq-coqprime]='/^Require Import PocklingtonRefl.$/ {print "From Coqprime "$0; next}'
 TEST_FILES[coq-coquelicot]='examples/BacS2013.v'
+PATCH_CMDS[coq-coquelicot]='/^Require Import .*Rcomplements.*.$/ {print "From Coquelicot "$0; next}'
+TEST_FILES[coq-corn]=''
+TEST_FILES[coq-deriving]='tests/tree.v'
+TEST_FILES[coq-dpdgraph]='tests/Test.v'
 TEST_FILES[coq-elpi]='examples/tutorial_coq_elpi_command.v examples/tutorial_elpi_lang.v'
-TEST_FILES[coq-elpi~dev]='examples/tutorial_coq_elpi.v examples/tutorial_elpi_lang.v'
-TEST_FILES[coq-elpi~8.13~2021.02]='examples/tutorial_coq_elpi.v examples/tutorial_elpi_lang.v'
 TEST_FILES[coq-elpi~8.12]='theories/examples/example_reflexive_tactic.v'
+TEST_FILES[coq-elpi~8.13~2021.02]='examples/tutorial_coq_elpi.v examples/tutorial_elpi_lang.v'
+TEST_FILES[coq-elpi~dev]='examples/tutorial_coq_elpi.v examples/tutorial_elpi_lang.v'
 TEST_FILES[coq-equations]='examples/Fin.v examples/STLC.v'
 TEST_FILES[coq-ext-lib]='examples/MonadReasoning.v examples/Printing.v'
 TEST_FILES[coq-flocq]='examples/Average.v' # In fixing: examples/Cody_Waite.v
 TEST_FILES[coq-flocq3]='examples/Average.v' # In fixing: examples/Cody_Waite.v
+PATCH_CMDS[coq-flocq3]='{gsub(/^From Flocq Require/, "From Flocq3 Require", $0); print $0; next}'
 TEST_FILES[coq-gappa]='testsuite/example-20101018.v testsuite/example-20090706.v testsuite/example-20080417.v'
+PATCH_CMDS[coq-gappa]='/^Require Import Gappa_tactic.$/ {print "From Gappa "$0; next}'
+TEST_FILES[coq-hammer-tactics]='/examples/tutorial/sauto/isort.v'
+TEST_FILES[coq-hammer]=''
 TEST_FILES[coq-hierarchy-builder]='examples/demo2/classical.v examples/demo2/stage10.v examples/demo2/stage11.v'
-TEST_FILES[coq-hierarchy-builder~8.13~2021.02]='demo2/classical.v demo2/stage10.v demo2/stage11.v'
 TEST_FILES[coq-hierarchy-builder~8.12]='demo2/classical.v demo2/stage10.v demo2/stage11.v'
+TEST_FILES[coq-hierarchy-builder~8.13~2021.02]='demo2/classical.v demo2/stage10.v demo2/stage11.v'
+TEST_FILES[coq-hott]='theories/Analysis/Locator.v'
+COQ_OPTION[coq-hott]='-noinit -indices-matter'
 TEST_FILES[coq-interval]='testsuite/example-20071016.v testsuite/example-20120205.v testsuite/example-20140221.v'
+TEST_FILES[coq-iris-heap-lang]='tests/heapprop.v'
+TEST_FILES[coq-iris]='tests/heapprop.v'
+TEST_FILES[coq-libhyps]='Demo/demo.v'
+TEST_FILES[coq-math-classes]=''
+TEST_FILES[coq-mathcomp-algebra-tactics]='examples/field_examples.v examples/ring_examples.v'
 TEST_FILES[coq-mathcomp-algebra]='mathcomp/algebra/finalg.v'
+TEST_FILES[coq-mathcomp-analysis]=''
 TEST_FILES[coq-mathcomp-bigenough]='bigenough.v'
 TEST_FILES[coq-mathcomp-character]='mathcomp/character/all_character.v'
 TEST_FILES[coq-mathcomp-field]='mathcomp/field/finfield.v'
 TEST_FILES[coq-mathcomp-fingroup]='mathcomp/fingroup/fingroup.v'
 TEST_FILES[coq-mathcomp-finmap]='finmap.v'
+TEST_FILES[coq-mathcomp-multinomials]=''
 TEST_FILES[coq-mathcomp-real-closed]='theories/complex.v'
 TEST_FILES[coq-mathcomp-solvable]='mathcomp/solvable/abelian.v'
 TEST_FILES[coq-mathcomp-ssreflect]='mathcomp/ssreflect/ssrbool.v'
-TEST_FILES[coq-mathcomp-algebra-tactics]='examples/field_examples.v examples/ring_examples.v'
+TEST_FILES[coq-mathcomp-zify]='examples/divmod.v examples/boolean.v'
 TEST_FILES[coq-menhirlib]='coq-menhirlib/src/Alphabet.v'
 TEST_FILES[coq-mtac2]='examples/basics_tutorial.v examples/tauto.v'
 TEST_FILES[coq-mtac2~8.12]='examples/tactics.v examples/tauto.v'
-TEST_FILES[coq-quickchick]='examples/PluginTest.v' # Check: BSTTest.v
-TEST_FILES[coq-simple-io]='test/Example.v test/TestExtraction.v'
-TEST_FILES[coq-unicoq]='test-suite/microtests.v'
-TEST_FILES[coq-vst]='progs64/reverse.v progs64/verif_reverse2.v'
-TEST_FILES[coq-vst~8.12]='progs/reverse.v progs/verif_reverse2.v'
-TEST_FILES[coq-vst-32]='progs/reverse.v progs/verif_reverse2.v'
-COQ_OPTION[coq-vst-32]='-Q $COQLIB/../coq-variant/VST32/VST VST -Q $COQLIB/../coq-variant/compcert32/compcert compcert'
-TEST_FILES[coq-hott]='theories/Analysis/Locator.v'
-COQ_OPTION[coq-hott]='-noinit -indices-matter'
-TEST_FILES[coq-reduction-effects]='tests/PrintEffect.v'
-TEST_FILES[coq-corn]=''
-TEST_FILES[coq-math-classes]=''
-TEST_FILES[coq-iris]='tests/heapprop.v'
-TEST_FILES[coq-iris-heap-lang]='tests/heapprop.v'
-TEST_FILES[coq-stdpp]='tests/sets.v'
-TEST_FILES[coq-record-update]='/tests/RecordSetTests.v'
-TEST_FILES[coq-hammer-tactics]='/examples/tutorial/sauto/isort.v'
-TEST_FILES[coq-hammer]=''
-TEST_FILES[coq-dpdgraph]='tests/Test.v'
+TEST_FILES[coq-ott]=''
 TEST_FILES[coq-paramcoq]='test-suite/Parametricity.v'
+TEST_FILES[coq-quickchick]='examples/PluginTest.v' # Check: BSTTest.v
+TEST_FILES[coq-record-update]='/tests/RecordSetTests.v'
+TEST_FILES[coq-reduction-effects]='tests/PrintEffect.v'
 TEST_FILES[coq-reglang]='theories/dfa.v'
-TEST_FILES[coq-mathcomp-analysis]=''
-TEST_FILES[coq-mathcomp-multinomials]=''
-TEST_FILES[coq-coqeal]='refinements/examples/irred.v'
-TEST_FILES[coq-coqprime]='src/Coqprime/examples/russell1.v'
-TEST_FILES[coq-coqprime-generator]=''
-TEST_FILES[coq-libhyps]='Demo/demo.v'
-TEST_FILES[coq-deriving]='tests/tree.v'
-TEST_FILES[coq-mathcomp-zify]='examples/divmod.v examples/boolean.v'
+TEST_FILES[coq-relation-algebra]='compiler_opts.v imp.v'
+PATCH_CMDS[coq-relation-algebra]='/^Require Import kat .*$/ {print "From RelationAlgebra "$0; next}'
 TEST_FILES[coq-serapi]=''
+TEST_FILES[coq-simple-io]='test/Example.v test/TestExtraction.v'
+TEST_FILES[coq-stdpp]='tests/sets.v'
+TEST_FILES[coq-unicoq]='test-suite/microtests.v'
 TEST_FILES[coq-unimath]='UniMath/Foundations/Tests.v'
 COQ_OPTION[coq-unimath]='-noinit -indices-matter -type-in-type -w -notation-overridden'
-TEST_FILES[coq-relation-algebra]='compiler_opts.v imp.v'
-TEST_FILES[coq-ott]=''
+TEST_FILES[coq-vst-32]='progs/reverse.v progs/verif_reverse2.v'
+COQ_OPTION[coq-vst-32]='-Q $COQLIB/../coq-variant/VST32/VST VST -Q $COQLIB/../coq-variant/compcert32/compcert compcert'
+PATCH_CMDS[coq-vst-32]='/^Require Import VST.progs.reverse.$/ {print "Require Import reverse. "; next}'
+TEST_FILES[coq-vst]='progs64/reverse.v progs64/verif_reverse2.v'
+PATCH_CMDS[coq-vst]='/^Require Import VST.progs64.reverse.$/ {print "Require Import reverse. "; next}'
+TEST_FILES[coq-vst~8.12]='progs/reverse.v progs/verif_reverse2.v'
+PATCH_CMDS[coq-vst~8.12]='/^Require Import VST.progs.reverse.$/ {print "Require Import reverse. "; next}'
 
 ##### Hacks for files #####
 
+# Patch a file using AWK
+# $1 = file name to patch
+# $2 = awk patch command
+
 function patch_file() {
-  awk '
-    /^Require Import Gappa_tactic.$/ {print "From Gappa "$0; next}
-    /^Require Import .*Rcomplements.*.$/ {print "From Coquelicot "$0; next}
-    /^Require Import PocklingtonRefl.$/ {print "From Coqprime "$0; next}
-    /^From HB.demo2 / {sub("From HB.demo2 ", "", $0); print $0; next}
-    /^Require Import Gappa_tactic.$/ {print "From Gappa "$0; next}
-	/^Require Import VST.progs64.reverse.$/ {print "Require Import reverse. "; next}
-	/^Require Import VST.progs.reverse.$/ {print "Require Import reverse. "; next}
-	/^Require Import kat .*$/ {print "From RelationAlgebra "$0; next}
-    {print $0}
-    ' $1 > $1.tmp
-  mv -f $1.tmp $1
+  if [ -n "$2" ]
+  then
+    echo "Patching file $1 with $2"
+    awk "$2"' {print $0}' $1 > $1.tmp
+    mv -f $1.tmp $1
+  fi
 }
 
 ##### Write header of bash runner script #####
@@ -254,6 +264,13 @@ do
     options=""
   fi
 
+  if [ -n "${PATCH_CMDS[${package}]+_undef_}" ]
+  then
+    patches="${PATCH_CMDS[${package}]}"
+  else
+    patches=""
+  fi
+
   if [ -n "$files" ]
   then
     # get installed version of package (otherwise opam source gives the latest)
@@ -267,7 +284,7 @@ do
       # coqc does not accept file names with -
       filename=${filename/-/_}
       cp "smoke-test-kit/${package}-src/$file" "smoke-test-kit/${package}/${filename}"
-      patch_file "smoke-test-kit/${package}/${filename}"
+      patch_file "smoke-test-kit/${package}/${filename}" "${patches}"
       echo "run_test ${package}/${filename} $options" >> $smoke_script
       echo "CALL :run_test ${package}/${filename} ${options//\$COQLIB/%COQLIB%}"$'\r' >> $smoke_batch
     done
