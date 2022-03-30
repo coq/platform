191901870a9a382502652a698eb115b663349496
Michael Soegtrop <7895506+MSoegtropIMC@users.noreply.github.com>
2022-03-30 12:16:46 +0200

    Smoke test: add back test file patches for gappa - old picks still need this

--- shell_scripts/create_smoke_test_kit.sh
+++ shell_scripts/create_smoke_test_kit.sh
@@ -78,6 +78,8 @@ TEST_FILES[coq-flocq]='examples/Average.v' # In fixing: examples/Cody_Waite.v
 TEST_FILES[coq-flocq3]='examples/Average.v' # In fixing: examples/Cody_Waite.v
 PATCH_CMDS[coq-flocq3]='{gsub(/^From Flocq Require/, "From Flocq3 Require", $0); print $0; next}'
 TEST_FILES[coq-gappa]='testsuite/example-20101018.v testsuite/example-20090706.v testsuite/example-20080417.v'
+# Note: the latest version of coq-gappa does not require patches, but versions in older picks do require it
+PATCH_CMDS[coq-gappa]='/^Require Import Gappa_tactic.$/ {print "From Gappa "$0; next}'
 TEST_FILES[coq-gappa-flocq3]='testsuite/example-20101018.v testsuite/example-20090706.v testsuite/example-20080417.v'
 PATCH_CMDS[coq-gappa-flocq3]='/^From Gappa Require/ {sub("From Gappa Require", "From GappaFlocq3 Require", $0); print $0; next} /^From Flocq Require/ {sub("From Flocq Require", "From Flocq3 Require", $0); print $0; next}'
 TEST_FILES[coq-hammer-tactics]='/examples/tutorial/sauto/isort.v'
