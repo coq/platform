84c59625d8e834a489a7639a1cf9496c5d593aaf
Michael Soegtrop <7895506+MSoegtropIMC@users.noreply.github.com>
2022-09-06 15:45:51 +0200

    Pick 2022.09~preview1: updated coq-ext-lib to version 0.11.7

--- package_picks/package-pick-8.16~2022.09~beta1.sh
+++ package_picks/package-pick-8.16~2022.09~beta1.sh
@@ -68,7 +68,7 @@ then
 
   # Standard library extensions
   PACKAGES="${PACKAGES} coq-bignums.8.16.0"
-  PACKAGES="${PACKAGES} coq-ext-lib.0.11.6"
+  PACKAGES="${PACKAGES} coq-ext-lib.0.11.7"
   PACKAGES="${PACKAGES} coq-stdpp.1.8.0"
 
   # General mathematics
