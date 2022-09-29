5484d404ae243dfe704c26f4622a180a32ebb480
Michael Soegtrop <7895506+MSoegtropIMC@users.noreply.github.com>
2022-09-29 19:56:49 +0200

    Pick 8.16~2022.09~beta1: disabled MetaCoq on Windows (does not compile)

--- package_picks/package-pick-8.16~2022.09~beta1.sh
+++ package_picks/package-pick-8.16~2022.09~beta1.sh
@@ -174,7 +174,10 @@ then
 
   # Proof automation / generation / helpers
   PACKAGES="${PACKAGES} coq-deriving.0.1.0"
-  PACKAGES="${PACKAGES} coq-metacoq.1.1+8.16"
+  if [ "$OSTYPE" != cygwin ]
+  then
+    PACKAGES="${PACKAGES} coq-metacoq.1.1+8.16"
+  fi
 
   # General mathematics
   PACKAGES="${PACKAGES} coq-extructures.0.3.1"
