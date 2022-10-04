a88d6c085b915b590c9599f2a74ab5c020477478
Michael Soegtrop <7895506+MSoegtropIMC@users.noreply.github.com>
2022-10-04 17:37:58 +0200

    Smoke test: updated smoke test files for fiat-crypto and its dependencies

--- test_files/coq-fiat-crypto/test.v
+++
@@ -1,3 +0,0 @@
-(* See https://github.com/coq/platform/issues/178#issuecomment-1211112651 *)
-Require Import Crypto.StandaloneOCamlMain.
-Check Crypto.StandaloneOCamlMain.UnsaturatedSolinas.main.
--- test_files/coq-rewriter/test.v
+++
@@ -1,4 +0,0 @@
-(* See https://github.com/coq/platform/issues/178#issuecomment-1211112651 *)
-Require Import Rewriter.Util.plugins.RewriterBuild.
-Require Import Coq.Arith.Arith.
-Make rew0nat := Rewriter For (Nat.add_0_r, Nat.add_0_l).
