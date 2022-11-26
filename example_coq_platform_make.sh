a0bbe5285ec684bf200d398978604be88aa22393
Michael Soegtrop <7895506+MSoegtropIMC@users.noreply.github.com>
2022-11-26 21:57:34 +0100

    Pick 8.16~2022.09: adjust example call files to this pick

--- example_coq_platform_make.sh
+++ example_coq_platform_make.sh
@@ -1,3 +1,3 @@
 # This is a test / example call for coq_platform_make.sh
 
-./coq_platform_make.sh -packages="8.15~2022.04" -extent=x -parallel=p -jobs=16 -compcert=y -large=i -switch=k
+./coq_platform_make.sh -packages="8.16~2022.09" -extent=x -parallel=p -jobs=8 -compcert=y -large=i -switch=k
