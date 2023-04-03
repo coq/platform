af923d87d93766b5d90fc0d7682658d24f159ccf
Michael Soegtrop <7895506+MSoegtropIMC@users.noreply.github.com>
2023-04-03 17:58:31 +0200

    Pick 8.17~2023.03+beta1: updated coq-fiat-crypto and prerequisites with local opam packages: - updated coq-coqutil to version 0.0.3 - updated coq-rewriter to version 0.0.8 - updated coq-riscv to version 0.0.4 - updated coq-bedrock2 to version 0.0.5 - updated coq-bedrock2-compiler to version 0.0.5 - updated coq-rupicola to version 0.0.7 - updated coq-fiat-crypto to version 0.0.20

--- package_picks/package-pick-8.17~2023.03+beta1.sh
+++ package_picks/package-pick-8.17~2023.03+beta1.sh
@@ -194,13 +194,13 @@ then
   then
     case "$COQ_PLATFORM_FIATCRYPTO" in
       [yY])
-        PACKAGES="${PACKAGES} coq-coqutil.0.0.2"
-        PACKAGES="${PACKAGES} coq-rewriter.0.0.7"
-        PACKAGES="${PACKAGES} coq-riscv.0.0.3"
-        PACKAGES="${PACKAGES} coq-bedrock2.0.0.4"
-        PACKAGES="${PACKAGES} coq-bedrock2-compiler.0.0.4"
-        PACKAGES="${PACKAGES} coq-rupicola.0.0.6"
-        # PACKAGES="${PACKAGES} coq-fiat-crypto.0.0.17" # fails to build
+        PACKAGES="${PACKAGES} coq-coqutil.0.0.3"
+        PACKAGES="${PACKAGES} coq-rewriter.0.0.8"
+        PACKAGES="${PACKAGES} coq-riscv.0.0.4"
+        PACKAGES="${PACKAGES} coq-bedrock2.0.0.5"
+        PACKAGES="${PACKAGES} coq-bedrock2-compiler.0.0.5"
+        PACKAGES="${PACKAGES} coq-rupicola.0.0.7"
+        PACKAGES="${PACKAGES} coq-fiat-crypto.0.0.20"
         ;;
       [nN]) true ;;
       *) echo "Illegal value for COQ_PLATFORM_FIATCRYPTO - aborting"; false ;;
