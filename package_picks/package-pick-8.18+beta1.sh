ab0d801de49bac1c0415e619c2e10df0e62fce94
Romain Tetley <romain.tetley@inria.fr>
2023-11-02 09:24:09 +0100

    Clean up

--- package_picks/package-pick-8.18+beta1.sh
+++ package_picks/package-pick-8.18+beta1.sh
@@ -172,7 +172,7 @@ then
   esac
 
   # # Proof analysis and other tools
-  PACKAGES="${PACKAGES} coq-dpdgraph.1.0+8.18" # does not build even after relaxing version constraints
+  PACKAGES="${PACKAGES} coq-dpdgraph.1.0+8.18"
 fi
 
 ########## EXTENDED" COQ PLATFORM PACKAGES ##########
