adbe49fa0646fdfd6273151da2c3eec09045fe13
Romain Tetley <romain.tetley@inria.fr>
2024-03-20 14:14:49 +0100

    Remove quickchick
    coq-quickchick seems to be broken on Windows

--- package_picks/package-pick-8.19~2024.01+beta1.sh
+++ package_picks/package-pick-8.19~2024.01+beta1.sh
@@ -124,7 +124,7 @@ then
   #PACKAGES="${PACKAGES} coq-mtac2.1.4+8.18" #DOES NOT BUILD, DEPENDS ON UNICOQ
   PACKAGES="${PACKAGES} elpi.1.18.1 coq-elpi.2.0.1"
   PACKAGES="${PACKAGES} coq-hierarchy-builder.1.7.0"
-  PACKAGES="${PACKAGES} coq-quickchick.2.0.2"
+  #PACKAGES="${PACKAGES} coq-quickchick.2.0.2" #DOES NOT BUILD ON WINDOWS
   #PACKAGES="${PACKAGES} coq-hammer-tactics.1.3.2+8.18" # DOES NOT BUILD
   if [[ "$OSTYPE" != cygwin ]]
   then
