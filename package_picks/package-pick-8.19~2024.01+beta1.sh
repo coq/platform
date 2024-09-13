345dd7618d89b387a231018d65dcafef9cb42414
Michael Soegtrop <7895506+MSoegtropIMC@users.noreply.github.com>
2024-09-13 14:36:31 +0200

    Windows: fixed Mtac2 path issues by a work around

--- package_picks/package-pick-8.19~2024.01+beta1.sh
+++ package_picks/package-pick-8.19~2024.01+beta1.sh
@@ -126,10 +126,7 @@ then
   PACKAGES="${PACKAGES} coq-equations.1.3+8.19"
   PACKAGES="${PACKAGES} coq-aac-tactics.8.19.0"
   PACKAGES="${PACKAGES} coq-unicoq.1.6+8.19"
-  if [[ "$OSTYPE" != cygwin ]]
-  then
-    PACKAGES="${PACKAGES} coq-mtac2.1.4+8.19" # build issues on Windows
-  fi
+  PACKAGES="${PACKAGES} coq-mtac2.1.4+8.19" # build issues on Windows
   PACKAGES="${PACKAGES} elpi.1.18.2 coq-elpi.2.1.0"
   PACKAGES="${PACKAGES} coq-hierarchy-builder.1.7.0"
   if [[ "$OSTYPE" != cygwin ]]
