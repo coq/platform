3af941712930497bac26a34b4eba3227a95dfb41
Michael Soegtrop <7895506+MSoegtropIMC@users.noreply.github.com>
2025-02-01 10:07:26 +0100

    Pick -8.19~2024.10: fixed typo in vscoq-language-sever line

--- package_picks/package-pick-8.19~2024.10.sh
+++ package_picks/package-pick-8.19~2024.10.sh
@@ -60,7 +60,7 @@ PACKAGES="${PACKAGES} PIN.coq.8.19.2"
 if  [[ "${COQ_PLATFORM_EXTENT}"  =~ ^[iIfFxX] ]]
 then
 PACKAGES="${PACKAGES} coqide.8.19.2"
-PACKAGES="${PACKAGES} vscoq-language-server 2.2.3"
+PACKAGES="${PACKAGES} vscoq-language-server.2.2.3"
 fi
 
 ########## "FULL" COQ PLATFORM PACKAGES ##########
